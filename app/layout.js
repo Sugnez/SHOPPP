be80d6f1d6211e4baaaf43d906a59ce11e9a7a02
Sugnez <89664093+Sugnez@users.noreply.github.com>
2025-04-14 00:51:52 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+// app/layout.js
+export const metadata = {
+  title: "Komat Boutique",
+  description: "Vintage Streetwear aus den 90ern & 2000ern",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
